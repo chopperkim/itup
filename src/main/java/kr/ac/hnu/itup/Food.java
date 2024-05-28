a7f8676870d8637f1f1b5c1f01a384a73e4f8a9b
chopperkim <chocojhkim@gmail.com>
2024-05-28 20:57:01 +0900

    initial commit

---
+++ src/main/java/kr/ac/hnu/itup/Food.java
@@ -0,0 +1,22 @@
+package kr.ac.hnu.itup;
+
+public class Food {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}

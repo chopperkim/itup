385f5f64d6fc20ec54a6381ed36e8f5f142d1e69
chopperkim <chocojhim@gmail.com>
2024-05-29 18:14:07 +0900

    System.out.println()에서 Slf4j로 로그 변경

--- src/main/java/kr/ac/hnu/itup/ItupApplication.java
+++ src/main/java/kr/ac/hnu/itup/ItupApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 
@@ -16,8 +15,8 @@ public class ItupApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(ItupApplication.class, args);
 		Restaurant restaurant = context.getBean("restaurant", Restaurant.class);
-//		log.info("Restaurant: {}", restaurant);
-		System.out.println("Restaurant: " + restaurant);
+		log.info("Restaurant: {}", restaurant);
+//		System.out.println("Restaurant: " + restaurant);
 	}
 
 }
